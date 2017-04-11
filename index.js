db10507cfe104b023004862fdbc8bf2ebb23e82f
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-11 22:06:23 +0800

    revert previous secret

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 // handle login/logout (session comes before passport)
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'savethecheerleadersavetheworld',
   cookie: {},
   resave: false,
   saveUninitialized: true,
