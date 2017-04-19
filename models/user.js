9e964fa02dae27899c96803a4313bce073e2f563
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-19 20:20:11 +0800

    fix special request

--- models/user.js
+++ models/user.js
@@ -19,6 +19,7 @@ var UserSchema = new mongoose.Schema({
   },
   name: {
     type: String,
+    required: true,
     minlength: [3, 'Name must be between 3 and 99 characters'],
     maxlength: [99, 'Name must be between 3 and 99 characters']
   },
